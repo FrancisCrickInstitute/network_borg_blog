8cca1b668b90a2175effae32e7a84ccb9ab50430
Paul Mahan <paul.mahan@crick.ac.uk>
2020-03-11 13:02:23 +0000

    Version 0.9.9.7

--- modules/_network_borg_ncclient.py
+++ modules/_network_borg_ncclient.py
@@ -86,7 +86,7 @@ def ncclient(SESSION_TK, YAML_TK, ncclient_mode, item, obj):
                 port=dport,
                 timeout=90,
                 hostkey_verify=False,
-                device_params={'name': driver}) as m:
+                device_params={'name':driver}) as m:
 
                 #for capabilities in m.server_capabilities:
                 #    print(capabilities)
